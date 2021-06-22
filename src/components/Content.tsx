159b6b14090963f7a38811a46c54a4a52268e6c4
Joao Vanelli <vanellijoao@gmail.com>
2021-06-22 11:12:12 -0300

    Finalizando Desafio 2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface Props {
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Runtime: string,
+    Ratings: {
+      Value: string,
+    }[], 
+  }[],
+  selectedGenre: {
+    title: string,
+  },
+}
+
+export function Content(props: Props) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
